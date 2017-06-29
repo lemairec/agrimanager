bb48f0935a39ab0480b6bc484f648eedd51cdd77
lemairec <lemairec02@gmail.com>
2017-06-29 23:21:36 +0200

    common controller

---
+++ src/AgriBundle/Controller/CommonController.php
@@ -0,0 +1,50 @@
+<?php
+namespace AgriBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class CommonController extends Controller
+{
+    public function check_user(){
+        if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            throw $this->createAccessDeniedException();
+        }
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->getUser();
+        $this->company = $em->getRepository('AgriBundle:Company')->findOrCreate($user);
+    }
+
+
+    public function getCurrentCampagneId($request){
+        $session = $request->getSession();
+        $campagne_id = $session->get('campagne_id', '');
+        if($campagne_id == ''){
+            $em = $this->getDoctrine()->getManager();
+            $campagne = $em->getRepository('AgriBundle:Campagne')->findAll()[0]->id;
+        }
+        $new_campagne_id = $request->query->get('campagne_id');
+        if($new_campagne_id != ''){
+            $session->set('campagne_id', $new_campagne_id);
+            return $new_campagne_id;
+        }
+
+        return $campagne_id;
+    }
+
+    public function getCurrentCampagne($request){
+        $campagne_id = $this->getCurrentCampagneId($request);
+        $em = $this->getDoctrine()->getManager();
+        $this->check_user();
+
+        $campagne = $em->getRepository('AgriBundle:Campagne')->findOneById($campagne_id);
+        if(!property_exists($this, 'campagnes')){
+            $this->campagnes = $em->getRepository('AgriBundle:Campagne')->getAllforCompany($this->company);
+        }
+
+        if($campagne){
+            return $campagne;
+        } else {
+            return $this->campagnes[0];
+        }
+    }
+}
--- src/AgriBundle/Controller/DefaultController.php
+++ src/AgriBundle/Controller/DefaultController.php
@@ -2,7 +2,6 @@
 
 namespace AgriBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,6 +10,8 @@ use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 
 use Datetime;
 
+
+use AgriBundle\Controller\CommonController;
 use AgriBundle\Entity\Achat;
 use AgriBundle\Entity\Ilot;
 use AgriBundle\Entity\Campagne;
@@ -37,51 +38,8 @@ use AgriBundle\Form\InterventionMaterielType;
 use AgriBundle\Form\MaterielEntretienType;
 use AgriBundle\Form\InterventionProduitType;
 
-class DefaultController extends Controller
+class DefaultController extends CommonController
 {
-    private function check_user(){
-        if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
-            throw $this->createAccessDeniedException();
-        }
-        $em = $this->getDoctrine()->getManager();
-        $user = $this->getUser();
-        $this->company = $em->getRepository('AgriBundle:Company')->findOrCreate($user);
-    }
-
-
-    private function getCurrentCampagneId($request){
-        $session = $request->getSession();
-        $campagne_id = $session->get('campagne_id', '');
-        if($campagne_id == ''){
-            $em = $this->getDoctrine()->getManager();
-            $campagne = $em->getRepository('AgriBundle:Campagne')->findAll()[0]->id;
-        }
-        $new_campagne_id = $request->query->get('campagne_id');
-        if($new_campagne_id != ''){
-            $session->set('campagne_id', $new_campagne_id);
-            return $new_campagne_id;
-        }
-
-        return $campagne_id;
-    }
-
-    private function getCurrentCampagne($request){
-        $campagne_id = $this->getCurrentCampagneId($request);
-        $em = $this->getDoctrine()->getManager();
-        $this->check_user();
-
-        $campagne = $em->getRepository('AgriBundle:Campagne')->findOneById($campagne_id);
-        if(!property_exists($this, 'campagnes')){
-            $this->campagnes = $em->getRepository('AgriBundle:Campagne')->getAllforCompany($this->company);
-        }
-
-        if($campagne){
-            return $campagne;
-        } else {
-            return $this->campagnes[0];
-        }
-    }
-
 
     /**
      * @Route("/", name="home")
