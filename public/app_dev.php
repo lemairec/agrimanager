49f8f587985e6d0df32387e7c642ee6de5f5fba2
Clement Lemaire <lemairec02@gmail.com>
2018-05-11 19:59:46 +0200

    remove warning

--- public/app_dev.php
+++ public/app_dev.php
@@ -22,8 +22,9 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
 $loader = require __DIR__.'/../app/autoload.php';
 Debug::enable();
 
+
+
 $kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
