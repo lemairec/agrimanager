adb63eb2a9dbdee83cec4e67fc7ac31d5d5c61ee
Clement Lemaire <lemairec02@gmail.com>
2018-05-27 21:28:31 +0200

    gasoils

--- src/AgriBundle/Controller/DefaultController.php
+++ src/AgriBundle/Controller/DefaultController.php
@@ -650,53 +650,6 @@ class DefaultController extends CommonController
         ));
     }
 
-    /**
-     * @Route("gasoils", name="gasoils")
-     */
-    public function gasoilsAction(Request $request)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $campagne = $this->getCurrentCampagne($request);
-
-        $gasoils = $em->getRepository('AgriBundle:Gasoil')->getAllForCampagne($campagne);
-        $gasoilsType = $em->getRepository('AgriBundle:Gasoil')->getAllforCompanyGroupByType($this->company);
-        return $this->render('AgriBundle:Default:gasoils.html.twig', array(
-            'campagnes' => $this->campagnes,
-            'campagne_id' => $campagne->id,
-            'gasoils' => $gasoils,
-            'gasoilsType' => $gasoilsType
-        ));
-    }
-
-    /**
-     * @Route("/gasoil/{gasoil_id}", name="gasoil")
-     **/
-    public function gasoilEditAction($gasoil_id, Request $request)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $campagne = $this->getCurrentCampagne($request);
-        $materiels = $em->getRepository('AgriBundle:Materiel')->getAllForCompany($this->company);
-        $materiels[] = null;
-        if($gasoil_id == '0'){
-            $gasoil = new Gasoil();
-            $gasoil->campagne = $campagne;
-            $gasoil->date = new \DateTime();
-        } else {
-            $gasoil = $em->getRepository('AgriBundle:Gasoil')->findOneById($gasoil_id);
-        }
-        $form = $this->createForm(GasoilType::class, $gasoil, ['materiels'=>$materiels]);
-        $form->handleRequest($request);
-
-
-        if ($form->isSubmitted()) {
-            $gasoil = $em->getRepository('AgriBundle:Gasoil')->save($gasoil);
-            return $this->redirectToRoute('gasoils');
-        }
-        return $this->render('AgriBundle:Default:gasoil.html.twig', array(
-            'form' => $form->createView()
-        ));
-    }
-
 
     /**
      * @Route("deplacements", name="deplacements")
