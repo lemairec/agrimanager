a29398e6b74e250268f14775a3f9eb1a19d1fd3c
Clement Lemaire <lemairec02@gmail.com>
2018-08-04 10:19:56 +0200

    Route warning

--- src/AppBundle/Controller/Bilan2Controller.php
+++ src/AppBundle/Controller/Bilan2Controller.php
@@ -2,7 +2,7 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
