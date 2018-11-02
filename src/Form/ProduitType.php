8b02e0d64faec98a32c6cdd977f6c7ba223a6e31
Clement Lemaire <lemairec02@gmail.com>
2018-11-02 16:05:16 +0100

    gui

--- src/Form/ProduitType.php
+++ src/Form/ProduitType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 
 class ProduitType extends AbstractType
@@ -14,9 +15,19 @@ class ProduitType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name')->add('type')->add('qty')->add('price')->add('unity')->add('ephyProduit');
+        $builder->add('name')->add('type', ChoiceType::class, array(
+            'choices'  => ["autre" => "autre"
+            , "agroequipements" => "agroequipements"
+            , "engrais" => "engrais"
+            , "oligos" => "oligos"
+            , "semences" => "semences"
+            , "phytos" => "phytos"
+            , "services" => "services"
+            , "lisa" => "lisa"]
+                ));
+        $builder->add('qty')->add('price')->add('unity')->add('ephyProduit');
         $builder->add('n')->add('p')->add('k')->add('s')->add('mg');
-        
+
     }
 
     /**
