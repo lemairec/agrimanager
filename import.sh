da842734932f3b068842ed71b8d3c1cd900459a3
Clement Lemaire <lemairec02@gmail.com>
2019-01-31 20:44:33 +0100

    Import

--- import.sh
+++ import.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
-dump=~/Downloads/dump_1546164665;
+dumps=~/Downloads/dump_*
+
+arrays=($(ls -d $dumps))
+arraylength=${#arrays[@]}
+
+# use for loop to read all values and indexes
+for (( i=0; i<${arraylength}; i++ ));
+do
+  echo $i " / " ${arraylength} " : " ${arrays[$i]}
+done
+
+read input
+dump=${arrays[$input]}
+
+echo "$dump, is ok?"
+read input
+
+
 agrimanager=~/workspace/agrimanager;
 
-rm -f $agrimanager/public/uploads/documents/*.pdf
+rm -f $agrimanager/public/uploads/documents/*
+cd $agrimanager/public/uploads/documents/
 
-cp $dump/*.pdf $agrimanager/public/uploads/documents/;
+unzip $dump
 
-bin/console doctrine:database:drop --force;
-bin/console doctrine:database:create;
-mysql --host localhost --user root --password maplaine < $dump/database.sql
+
+#cp $dump/*.pdf $agrimanager/public/uploads/documents/;
+
+$agrimanager/bin/console doctrine:database:drop --force;
+$agrimanager/bin/console doctrine:database:create;
+mysql --host localhost --user root --password maplaine < database.sql
