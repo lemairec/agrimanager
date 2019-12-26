791b30374ce84b848de82a864dedc2095e37efb7
Clement Lemaire <lemairec02@gmail.com>
2019-12-26 11:30:21 +0100

    ORM\

--- src/Entity/Intervention.php
+++ src/Entity/Intervention.php
@@ -3,79 +3,70 @@
 namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 
-use Doctrine\ORM\Mapping\Entity;
-use Doctrine\ORM\Mapping\Table;
-use Doctrine\ORM\Mapping\Id;
-use Doctrine\ORM\Mapping\Column;
-use Doctrine\ORM\Mapping\GeneratedValue;
-use Doctrine\ORM\Mapping\ManyToOne;
-use Doctrine\ORM\Mapping\OneToMany;
-use Doctrine\ORM\Mapping\JoinColumn;
-
 /**
  * Intervention
  *
- * @Table(name="intervention")
- * @Entity(repositoryClass="App\Repository\InterventionRepository")
+ * @ORM\Table(name="intervention")
+ * @ORM\Entity(repositoryClass="App\Repository\InterventionRepository")
  */
 class Intervention
 {
     /**
      * @var guid
      *
-     * @Column(name="id", type="guid")
-     * @Id
-     * @GeneratedValue(strategy="UUID")
+     * @ORM\Column(name="id", type="guid")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="UUID")
      */
     public $id;
 
     /**
-     * @ManyToOne(targetEntity="App\Entity\Campagne")
-     * @JoinColumn(nullable=false)
+     * @ORM\ManyToOne(targetEntity="App\Entity\Campagne")
+     * @ORM\JoinColumn(nullable=false)
      */
     public $campagne;
 
     /**
-     * @ManyToOne(targetEntity="App\Entity\Company")
-     * @JoinColumn(nullable=false)
+     * @ORM\ManyToOne(targetEntity="App\Entity\Company")
+     * @ORM\JoinColumn(nullable=false)
      */
     public $company;
 
     /**
-     * @var \DateTime
+     * @ORM\var \DateTime
      *
-     * @Column(name="date", type="date")
+     * @ORM\Column(name="date", type="date")
      */
     public $date;
 
     /**
-     * @var string
+     * @ORM\var string
      *
-     * @Column(name="type", type="string", length=255)
+     * @ORM\Column(name="type", type="string", length=255)
      */
     public $type;
 
-    /** @Column(type="float") **/
+    /** @ORM\Column(type="float") **/
     public $surface;
 
-    /** @Column(type="string", length=255, nullable=true) **/
+    /** @ORM\Column(type="string", length=255, nullable=true) **/
     public $name;
 
-    /** @Column(type="text", nullable=true) **/
+    /** @ORM\Column(type="text", nullable=true) **/
     public $comment;
 
     /**
-    * @OneToMany(targetEntity="App\Entity\InterventionParcelle", mappedBy="intervention",cascade={"persist"})
+    * @ORM\OneToMany(targetEntity="App\Entity\InterventionParcelle", mappedBy="intervention",cascade={"persist"})
     */
     public $parcelles;
 
     /**
-    * @OneToMany(targetEntity="App\Entity\InterventionProduit", mappedBy="intervention",cascade={"persist"})
+    * @ORM\OneToMany(targetEntity="App\Entity\InterventionProduit", mappedBy="intervention",cascade={"persist"})
     */
     public $produits;
 
     /**
-    * @OneToMany(targetEntity="App\Entity\InterventionMateriel", mappedBy="intervention",cascade={"persist"})
+    * @ORM\OneToMany(targetEntity="App\Entity\InterventionMateriel", mappedBy="intervention",cascade={"persist"})
     */
     public $materiels;
 
