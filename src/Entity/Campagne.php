7e57f023c27e8c0a9af70b712035fbff4d54e591
Clement Lemaire <lemairec02@gmail.com>
2019-12-26 22:25:16 +0100

    Campagne

--- src/Entity/Campagne.php
+++ src/Entity/Campagne.php
@@ -5,8 +5,6 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * Campagne
- *
  * @ORM\Entity(repositoryClass="App\Repository\CampagneRepository")
  * @ORM\Table(name="campagne")
  */
@@ -21,6 +19,11 @@ class Campagne
      */
     public $id;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Company")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    public $company;
 
     /** @ORM\Column(type="string", length=255) **/
     public $name;
@@ -31,12 +34,7 @@ class Campagne
     /** @ORM\Column(name="color", type="string", length=25, nullable=true) **/
     public $color;
 
-    /**
-     * @ORM\ManyToOne(targetEntity="App\Entity\Company")
-     * @ORM\JoinColumn(nullable=false)
-     */
-    public $company;
-
+    
     public function __toString ( ){
         return $this->name;
     }
