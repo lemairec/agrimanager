f7d4624f302caa3c47ac843ee70762bbc46e4e04
Clément Lemaire <lemairec@unknown.home>
2020-01-24 21:16:54 +0100

    Upgrade

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
