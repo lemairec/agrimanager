899d3da791c7b6badcb493030038fc37361c7171
lemaire <lemaire@abvent.fr>
2021-09-23 17:21:47 +0200

    Robot job

---
+++ src/Entity/Robot/Job.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Entity\Robot;
+
+use App\Repository\Robot\JobRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=JobRepository::class)
+ */
+class Job
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    public $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    public $name;
+
+     /**
+     * @ORM\Column(type="type", length=255)
+     */
+    public $type;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    public $params = [];
+
+    public $params_json = "";
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Robot::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    public $robot;
+}
---
+++ src/Form/Robot/JobType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form\Robot;
+
+use App\Entity\Robot\Job;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class JobType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('type')
+            ->add('params_json')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Job::class,
+        ]);
+    }
+}
