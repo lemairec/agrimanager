c84ef8d05efdc2ec350c3df88592eda8d3e9d4c8
lemaire <lemaire@abvent.fr>
2022-08-10 12:44:35 +0200

    Zip end

--- src/Command/ExportBddCommand.php
+++ src/Command/ExportBddCommand.php
@@ -51,7 +51,7 @@ class ExportBddCommand extends Command
         $output->writeln("begin");
 
         //remove old
-        $output->writeln("remove");
+        $output->writeln("remove $path");
         $command = "rm -rf $path";
         $process= new Process($command);
         $process->run();
@@ -62,7 +62,7 @@ class ExportBddCommand extends Command
 
 
         //remove old
-        $output->writeln("mkdir");
+        $output->writeln("mkdir $path");
         $command = "mkdir -p $path";
         $process= new Process($command);
         $process->run();
@@ -87,49 +87,18 @@ class ExportBddCommand extends Command
         if (!$process->isSuccessful()) {
             throw new ProcessFailedException($process);
         }
-
-        #$output->writeln($backupSqlFile);
         $output->writeln('sql ok');
 
-        $output->writeln("create zip");
-        $zip = new \ZipArchive();
-        $zipName = "$path/dump_maplaine.zip";
-        $zip->open($zipName,  \ZipArchive::CREATE);
-        $output->writeln("file $zipName");
-        
-        $output->writeln("documents");
-        foreach ($this->em->getRepository('App:Document')->findAll() as $f) {
-            $file = $f->getDocName();
-            if($file){
-                $src = $this->projectDir."/public/uploads/documents/".$file;
-                #$output->writeln($src);
-                $zip->addFile($src, $file);
-            }
-        }
 
-        $output->writeln("factures");
-        $zip->addEmptyDir("factures");
-        $factures = $this->em->getRepository('App:Gestion\FactureFournisseur')->findAll();
-        $len = count($factures);
-        $i = 0;
-        foreach ($this->em->getRepository('App:Gestion\FactureFournisseur')->findAll() as $f) {
-            $file = $f->getFactureFileName();
-            if($file){
-                $src = $this->projectDir."/public/uploads/factures/".$file;
-                #$output->writeln($src);
-                $zip->addFile($src, "factures/".$file);
-            }
-            $i = $i+1;
-            if($i%($len/10) == 0){
-                $perc = round($i/($len/100));
-                $output->writeln("factures $perc %");
-            }
+        $output->writeln("copie");
+        $command = "cp -r ~/maplaine/public/uploads/* $path";
+        $process= new Process($command);
+        $process->run();
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
         }
-        $output->writeln("sql");
-        
-        $zip->addFile($backupSqlFile, "database.sql");
-        $output->writeln("close");
-        $zip->close();
+        $output->writeln("copie ok");
+       
     }
 
 }
