d22416396982a22ecf54b879b09776f572b2461d
lemaire <lemaire@abvent.fr>
2022-08-10 13:19:09 +0200

    Import

--- import.sh
+++ import.sh
@@ -1,36 +1,21 @@
 #!/bin/bash
 
-dumps=~/workspace/dump/*
-
-arrays=($(ls -d $dumps))
-arraylength=${#arrays[@]}
-
-# use for loop to read all values and indexes
-for (( i=0; i<${arraylength}; i++ ));
-do
-  echo $i " / " ${arraylength} " : " ${arrays[$i]}
-done
-
-read input
-dump=${arrays[$input]}
-
-echo "$dump, is ok?"
-read input
-
-
 agrimanager=~/workspace/maplaine;
 
-rm -f $agrimanager/public/uploads/documents/*
-rm -f $agrimanager/public/uploads/factures/*
+rm -rf ~/workspace/dump/;
 
-cd $agrimanager/public/uploads/documents/
-unzip $dump
+rsync maplainemk@ssh.cluster023.hosting.ovh.net:maplaine/temp/dump ~/workspace/ --progress -ah
 
-mv $agrimanager/public/uploads/documents/factures/* $agrimanager/public/uploads/factures/
+rm -rf $agrimanager/public/uploads/*
 
 
-#cp $dump/*.pdf $agrimanager/public/uploads/documents/;
+echo "copie"
+cp -r ~/workspace/dump/* $agrimanager/public/uploads/
+echo "copie end"
 
+echo "sql"
+cd $agrimanager/public/uploads/
 $agrimanager/bin/console doctrine:database:drop --force;
 $agrimanager/bin/console doctrine:database:create;
-docker exec -i mariadb mysql -uroot -pzeouane maplaine < database.sql
+docker exec -i mariadb mysql -uroot -pzeouane maplaine < backup.sql
+echo "sql end"
