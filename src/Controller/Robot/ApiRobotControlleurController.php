6bfcdd9f538e0cac2821360c1dcd46df9ef3bfc7
lemairec <lemairec02@gmail.com>
2022-11-21 22:10:08 +0100

    Fix

--- src/Controller/Robot/ApiRobotControlleurController.php
+++ src/Controller/Robot/ApiRobotControlleurController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 use App\Entity\Robot\Robot;
+use App\Entity\Robot\Order;
 
 
 class ApiRobotControlleurController extends CommonController
