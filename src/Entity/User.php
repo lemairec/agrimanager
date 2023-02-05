5802519ad294d3b325298cd5f2da09c9b0896336
lemairec <lemairec02@gmail.com>
2023-02-05 19:14:54 +0100

    Register

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,10 +5,12 @@ namespace App\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Uid\Uuid;
 
 #[ORM\Table(name: '_fos_user')]
 #[ORM\Entity(repositoryClass: 'App\Repository\UserRepository')]
+#[UniqueEntity(fields: ['username'], message: 'There is already an account with this username')]
 class User extends BaseUser
 {
     #[ORM\Id]
@@ -37,11 +39,26 @@ class User extends BaseUser
 
     public $show_unity = true;
 
+    #[ORM\Column(type: 'boolean')]
+    private $isVerified = false;
+
 
     public function __construct()
     {
         parent::__construct();
         $this->id = Uuid::v4();
         // your own logic
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): self
+    {
+        $this->isVerified = $isVerified;
+
+        return $this;
     }
 }
