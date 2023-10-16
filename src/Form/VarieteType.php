e33b36876cf34c351fc588daaec627538e8a8c37
lemairec <lemairec02@gmail.com>
2023-10-16 22:49:01 +0200

    Variété

--- src/Form/VarieteType.php
+++ src/Form/VarieteType.php
@@ -2,6 +2,7 @@
 
 namespace App\Form;
 
+use App\Entity\Culture;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -20,7 +21,7 @@ class VarieteType extends AbstractType
         ));
         $builder->add('surface');
         $builder->add('comment');
-        
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
